fe1965babd741d69b429c88936ad708ab03cb950
Tertuh <gabriel.trtl@gmail.com>
2023-07-05 12:25:29 -0300

    v3.0

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-function validator() {
+function formvalidator() {
     // Pega o valor do campo de input com a id="mailform"
-    let mail = document.getElementById("mailform");
-    let = 
-    // 
+    let email = document.getElementById("mailform").value;
+    // Seleciona o elemento com a id="error", neste caso, é um elemento <p></p>
+    let error = document.getElementById("error");
+
+    if (email === '') {
+        error.textContext = "Please provide a valid email";
+    } else if (email.indexOF('@') === -1 || email.indexof('.') === -1) {
+        error.textContent = "Please provide a valid email";
+    } else {
+        error.textContent = ''; // Limpar mensagem de erro se for válido
+        alert('E-mail Válido: ' + email);
+    }
+
 
 }
